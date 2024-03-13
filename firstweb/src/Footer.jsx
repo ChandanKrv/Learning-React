5f9294c452abe85e6a05436b4c84ab3964b946f4
ChandanKrv <chandan.nit.cse@gmail.com>
2024-03-13 10:46:14 +0530

    initial commit

---
+++ firstweb/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function footer(props) {
+    return (
+        <div>
+            <u>This is Footer Section</u>
+        </div>
+    );
+}
+
+export default footer;
---
+++ firstweb/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div>
+            <h1>This is Header Section from Header.jsx</h1>
+           
+        </div>
+    );
+}
+
+export default Header;
