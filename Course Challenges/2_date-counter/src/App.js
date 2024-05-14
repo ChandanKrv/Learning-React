d047974d1147b20e053a9a1e6580b5a98fd8a44b
ChandanKrv <chandan.nit.cse@gmail.com>
2024-05-14 17:50:37 +0530

    initial commit

--- Course Challenges/2_date-counter/src/App.js
+++ Course Challenges/2_date-counter/src/App.js
@@ -5,23 +5,34 @@ export default function App() {
   const[myStep,setMyStep] = useState(1);
   const[myCount,setMyCount]=useState(0);
 
+  function handleReset() {
+    setMyCount(0)
+    setMyStep(1)
+}
+
 const date = new Date()
   date.setDate(date.getDate() + myCount);
 
   return (
     <>
-    <div className="cn">
-      <button onClick={()=>myStep>1 ? setMyStep(myStep-1): ""}> - </button>
+      <div className="cn">
+        <div>
+          <input type="range" min='0' max='10' value={myStep} onChange={(e) => setMyStep(Number(e.target.value))} />
+          <span>Step: { myStep}</span>
+        </div>
+            
+
+   {/*    <button onClick={()=>myStep>1 ? setMyStep(myStep-1): ""}> - </button>
         <span> Step: {myStep} </span>
-      <button onClick={()=>setMyStep((c)=>c+1)}> + </button>       
+      <button onClick={()=>setMyStep((c)=>c+1)}> + </button>    */}    
     </div>
 
     <div className="cn">
-      <button onClick={()=>setMyCount((c)=>c-myStep)}> - </button>
-        <span> Count: {myCount} </span>
+      <button onClick={()=>setMyCount((c)=>c-myStep)}> - </button>        
+        <input type="text" value={myCount} onChange={(e)=>setMyCount(Number(e.target.value))}/>
       <button onClick={()=>setMyCount((c)=>c+myStep)}> + </button>
       </div>
-
+        <span> Count: {myCount} </span>
       <div className="cn">
         <p>
           <span>{myCount === 0
@@ -34,6 +45,10 @@ const date = new Date()
           
         </p>
 
+        { (myCount!==0 || myStep !== 1) ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>):null } 
     </div>
     </>
   );
