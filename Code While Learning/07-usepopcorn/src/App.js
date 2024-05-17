a543bebaf6e03c88107dbc4e95db308875058033
ChandanKrv <chandan.nit.cse@gmail.com>
2024-05-18 00:28:21 +0530

    initial commit

--- Code While Learning/07-usepopcorn/src/App.js
+++ Code While Learning/07-usepopcorn/src/App.js
@@ -52,6 +52,8 @@ const average = (arr) =>
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData); 
+  
   return (
     <>
       <NavBar>        
@@ -60,10 +62,16 @@ export default function App() {
       </NavBar>
 
       <Main movies={movies}>
-        <ListBox>
+        <Box>
           <MovieList movies={movies} />
-        </ListBox>
-        <WatchedBox />      
+        </Box>
+
+        <Box>
+          <>
+            <WatchedSummary watched={watched} />
+            <WatchedMoviesList watched={watched} /> 
+          </>  
+        </Box>
       </Main>
      
     </>
@@ -119,22 +127,45 @@ function Main({children}) {
   )
 }
 
-function ListBox({children}) {
-  
-  const [isOpen1, setIsOpen1] = useState(true);
+function Box({children}){   
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
      <div className="box">
           <button
             className="btn-toggle"
-            onClick={() => setIsOpen1((open) => !open)}
+            onClick={() => setIsOpen((open) => !open)}
           >
-            {isOpen1 ? "–" : "+"}
+            {isOpen ? "–" : "+"}
           </button>
-          {isOpen1 && children}
+          {isOpen && children}
         </div>
   )
 }
+
+/* function WatchedBox() {
+  const [watched, setWatched] = useState(tempWatchedData); 
+  const [isOpen2, setIsOpen2] = useState(true);
+   
+  return (
+    <div className="box">
+          <button
+            className="btn-toggle"
+            onClick={() => setIsOpen2((open) => !open)}
+          >
+            {isOpen2 ? "–" : "+"}
+          </button>
+          {isOpen2 && (
+            <>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched}/>              
+            </>
+          )}
+        </div>
+  )
+}
+ */
+
 
 function MovieList({movies}) {
   
@@ -160,27 +191,6 @@ function Movie({movie}) {
   </li>)
 }
 
-function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData); 
-  const [isOpen2, setIsOpen2] = useState(true);
-   
-  return (
-    <div className="box">
-          <button
-            className="btn-toggle"
-            onClick={() => setIsOpen2((open) => !open)}
-          >
-            {isOpen2 ? "–" : "+"}
-          </button>
-          {isOpen2 && (
-            <>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched}/>              
-            </>
-          )}
-        </div>
-  )
-}
 
 function WatchedSummary({watched}) {  
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
--- Code While Learning/07-usepopcorn/src/index.js
+++ Code While Learning/07-usepopcorn/src/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+/* import './index.css';
 import App from './App';
+ */
+import StarRating from './StarRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color='green' maxRating={20} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*  <App /> */}
+    <StarRating messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+    
+
+    <StarRating maxRating={10} color="blue" size={34} className="test"
+      defaultRating={3}     
+    />
+    <Test/>
   </React.StrictMode>
 );
 
