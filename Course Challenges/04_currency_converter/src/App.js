069316f0bececb5193a2adae4cd55c56a0307e8b
ChandanKrv <chandan.nit.cse@gmail.com>
2024-05-26 21:09:20 +0530

    initial commit

--- Course Challenges/04_currency_converter/src/App.js
+++ Course Challenges/04_currency_converter/src/App.js
@@ -7,30 +7,45 @@ export default function App() {
   const [amount, setAmount] = useState(1);
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("INR");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
-   const res= fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`)
-    const data = res.json();
-  }, [])
+    async function convert() {
+      setIsLoading(true)
+      const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`)
+      const data = await res.json();
+      setConverted(data.rates[toCurrency])
+      setIsLoading(false);
+    }
+    if (fromCurrency === toCurrency)
+      return setConverted(amount)
+    convert();
+  }, [amount, fromCurrency,toCurrency]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Currency Converter</h1>
         <div>
-          <input type="number" value={amount} onChange={e=>setAmount(Number(e.target.value))} />
-          <select>
+          <input type="number"
+            value={amount}
+            onChange={e => setAmount(Number(e.target.value))} 
+            disabled={isLoading}
+
+            />
+          <select value={fromCurrency} onChange={e=>setFromCurrency(e.target.value)} disabled={isLoading}>
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
             <option value="INR">INR</option>
           </select>
            to  
-           <select>
+           <select value={ toCurrency} onChange={e=>setToCurrency(e.target.value)} disabled={isLoading}>
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
             <option value="INR">INR</option>
           </select>
-          <p>OUTPUT: {data.amount} </p>
+          <p>OUTPUT: {converted} </p>
         </div>
       </header>
     </div>
