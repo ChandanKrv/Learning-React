fb53846191e56a3de8dd5fe4c431c72370274eba
ChandanKrv <chandan.nit.cse@gmail.com>
2024-06-27 12:54:05 +0530

    completed

--- Code While Learning/10-react-quiz/src/components/DateCounter_old.js
+++ Code While Learning/10-react-quiz/src/components/DateCounter_old.js
@@ -1,18 +1,32 @@
 import { useReducer, useState } from "react";
 
 function reducer(state,action){
-  console.log(state,action)
-  if(action.type=='inc') return state + 1;
+  console.log(state, action)
+   
+
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - 1 };
+    case "inc":
+      return { ...state, count: state.count + 1 };
+    case "setCount":
+      return { ...state, count: action.payload };
+    default:
+      throw new Error("Unknown action");
+  }
+
+/*   if(action.type=='inc') return state + 1;
   if(action.type=='dec') return state - 1;
-  if(action.type=='setCount') return action.payload;
+  if(action.type=='setCount') return action.payload; */
 }
 
 function DateCounter() {
   //const [count, setCount] = useState(0);
 
-  const[count, dispatch]=useReducer(reducer,0)
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const [step, setStep] = useState(1);
+  const [count, step] = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
