1f4ec34b1157fe9fababe8bf531625716c9f5437
ChandanKrv <chandan.nit.cse@gmail.com>
2024-07-04 09:39:34 +0530

    initial commit

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100">
+      <div>
+        <h1>Hello Chandan</h1>
+      </div>
+    </main>
+  );
+  
+}
