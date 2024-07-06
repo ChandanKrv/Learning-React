2e5da8c1ec7cd00ff95af407b99701340f24a7e5
ChandanKrv <chandan.nit.cse@gmail.com>
2024-07-06 21:39:26 +0530

    initial commit

--- swiggy-clone/src/App.jsx
+++ swiggy-clone/src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="text-9xl">Hello123</div>;
+    </>
+  );
 }
 
-export default App
+export default App;
