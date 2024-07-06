2aa2514631bf60cc4b37aeea55dce436839c7083
ChandanKrv <chandan.nit.cse@gmail.com>
2024-07-06 16:43:22 +0530

    Mobile Responsive Done

--- Portfolio3/portfolio-react/src/Components/Navbar/Navbar.jsx
+++ Portfolio3/portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,39 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.svg";
 import underline from "../../assets/nav_underline.svg";
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+  };
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
 
   return (
     <div className="navbar">
       <img src={logo} alt="Logo" />
-      <ul className="nav-menu">
+      <img
+        src={menu_open}
+        onClick={openMenu}
+        alt="menu_open"
+        className="nav-mob-open"
+      />
+      <ul ref={menuRef} className="nav-menu">
+        <img
+          src={menu_close}
+          onClick={closeMenu}
+          alt="menu_close"
+          className="nav-mob-close"
+        />
+
         <li>
           <AnchorLink className="anchor-link" href="#home">
             <p onClick={() => setMenu("home")}>Home</p>
