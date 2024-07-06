861bd445223942617158b16cb799dcfd808d9ff4
ChandanKrv <chandan.nit.cse@gmail.com>
2024-07-06 15:29:03 +0530

    emailing feature added

--- Portfolio3/portfolio-react/src/Components/Contact/Contact.jsx
+++ Portfolio3/portfolio-react/src/Components/Contact/Contact.jsx
@@ -3,8 +3,37 @@ import theme_pattern from "../../assets/theme_pattern.svg";
 import mail_icon from "../../assets/mail_icon.svg";
 import location_icon from "../../assets/location_icon.svg";
 import call_icon from "../../assets/call_icon.svg";
+import { useState } from "react";
 
 const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "d9ee898a-0325-4097-a4b1-8a2a7123f0be");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      alert("Email sent successfully!");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+      alert("Something went wrong");
+    }
+    console.log(result);
+  };
+
   return (
     <div id="contact" className="contact">
       <div className="contact-title">
@@ -34,7 +63,7 @@ const Contact = () => {
           </div>
         </div>
 
-        <form className="contact-right">
+        <form onSubmit={onSubmit} className="contact-right">
           <label htmlFor="">Your Name</label>
           <input type="text" placeholder="Enter your name" name="name" />
 
